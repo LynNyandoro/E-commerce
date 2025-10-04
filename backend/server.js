d6427a0cd2dc5a4847a0c2d1f9b06a044109ecdc
Lyn Nyandoro <nyandorolyn@gmail.com>
2025-10-04 21:02:25 +0200

    Fix Express route pattern error: change '*' to '/*' for catch-all route

--- backend/server.js
+++ backend/server.js
@@ -122,7 +122,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
   
   // Catch all handler: send back React's index.html file for client-side routing
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 } else {
